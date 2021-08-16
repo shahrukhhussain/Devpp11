8010bb6e882f8f15482df6c3fe8760853af4e97c
Shahrukh <hussainshahrukh16@gmail.com>
2021-08-17 02:11:59 +0530

    Todo Mini Project Using React

--- Module1/lec1/intro.js
+++ Module1/lec1/intro.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+//console.log("Hello World");
+// console.log("Hello World !!!");
+
+// Data Types =>
+
+// Java => Primitive => int , float , double , boolean
+// Non Primitive => Arrays , Stacks
+
+
+// Javascript => Number(int , float , double) , Boolean , String(  "" , '' ) , undefined , Null , Object
+
+ // Datatype name = value;
+ // int a = 10;
+
+
+ // ES6 => Ecma Script 6
+// Two keyword let and const
+
+// Dynamic casting
+let a = 10;  // you have declared a variable with name and initialized it with value 10;
+// let keyword => block scoped
+
+ console.log(a);
+
+if(true){
+    a = 20;
+   console.log(a);
+}
+
+ console.log(a);
